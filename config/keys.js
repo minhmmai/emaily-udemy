b7f41cf8730491b5c37b7c9e2f12c94f0446e0a3
Minh Mai <minhmanmai90@gmail.com>
2019-06-16 13:26:37 +1000

    fix  secret and add default route

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 // Figure out what set of credentials to return
 if (process.env.NODE_ENV === "production") {
-// This is production environment => return prod keys set
-module.exports = require("./prod");
-}else {
-// This is dev environment => return dev keys set
-module.exports = require("./dev");
+    // This is production environment => return prod keys set
+    module.exports = require("./prod");
+} else {
+    // This is dev environment => return dev keys set
+    module.exports = require("./dev");
 }
