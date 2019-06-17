aec8efdd2accba35ecd5032dcc6b93f90666c92a
Minh Mai <minhmanmai90@gmail.com>
2019-06-17 20:53:34 +1000

    initial client setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import {Provider} from 'react-redux';
+import {createStore,applyMiddleware} from 'redux'; 
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Provider store = {store}><App/></Provider>,
+    document.querySelector('#root')
 );
