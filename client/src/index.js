0453af0618b6ebe6f6e963642d4792106f3abc0f
Minh Mai <minhmanmai90@gmail.com>
2019-08-01 22:51:18 +1000

    create-react-app client and concurrently dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore,applyMiddleware} from 'redux'; 
-import App from './components/App';
-import reducers from './reducers';
-
-const store = createStore(reducers, {}, applyMiddleware());
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
-    <Provider store = {store}><App/></Provider>,
-    document.querySelector('#root')
+  <App />,
+  document.getElementById('root')
 );
