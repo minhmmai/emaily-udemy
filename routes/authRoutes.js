ce788479f1e8db4cb189ad7e621130c993061801
Minh Mai <minhmanmai90@gmail.com>
2019-08-08 16:53:53 +1000

    installed client react app with create-react-app

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,13 +3,13 @@ const passport = require("passport");
 module.exports = (app) => {
     app.get('/', (req, res) => {
         res.send("Welcome to our app!");
-      });
+    });
 
     app.get("/auth/google", passport.authenticate("google", {
         scope: ["profile", "email"]
     }))
-    
-    app.get("/auth/google/callback", passport.authenticate("google"));
+
+    app.get("/auth/google/callback", passport.authenticate('google'))
 
     app.get("/api/current_user", (req, res) => {
         res.send(req.user);
